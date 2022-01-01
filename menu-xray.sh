396012ad44015df36abba684e9a957b18fb346ae
adamtech <adamtech323@gmail.com>
2022-01-01 18:07:29 +0800

    Update menu-xray.sh

--- menu-xray.sh
+++ menu-xray.sh
@@ -9,16 +9,21 @@ echo -e "        ░▓  ░ ▒░   ▒ ▒  ▒ ░   ░ ▒░▒░▒░ 
 echo -e "         ▒ ░░ ░░   ░ ▒░ ░       ░ ▒ ▒░       " |lolcat
 echo -e "         ▒ ░   ░   ░ ░  ░ ░   ░ ░ ░ ▒        " |lolcat
 echo -e "         ░           ░            ░ ░        " |lolcat
-echo ""
-
-echo -e "\e[1;31m* [1]\e[0m \e[1;32m: Create XRAY Vless\e[0m"
-echo -e "\e[1;31m* [2]\e[0m \e[1;32m: Deleting XRAY Vless\e[0m"
-echo -e "\e[1;31m* [3]\e[0m \e[1;32m: Extending Vless Account Active Life\e[0m"
 echo -e ""
-echo -e ""
-read -p "        Select From Options [1-3 or x]: " menuxray
-echo -e ""
-case $menuxray in
+echo -e  "  ╔═══════════════════════════════════════════════╗" | lolcat
+echo -e  "  ║                      XRAY                     ║" | lolcat
+echo -e  "  ╠═══════════════════════════════════════════════╝" | lolcat
+echo -e  "  ║" | lolcat
+echo -e  "  ║  [ 1 ] Create XRAY Vless"
+echo -e  "  ║  [ 2 ] Deleting XRAY Vless"
+echo -e  "  ║  [ 3 ] Extending Vless Account Active Life"
+echo -e  "  ║" | lolcat
+echo -e  "  ╠═══════════════════════════════════════════════╗" | lolcat
+echo -e  "  ║ - CTRL C to cancel                            ║" | lolcat
+echo -e  "  ╚═══════════════════════════════════════════════╝" | lolcat
+echo -e "\e[1;32m"
+read -p "     Please select an option :  "  XRAY
+echo -e "\e[0m"case $menuxray in
 1)
 add-xray
 ;;
